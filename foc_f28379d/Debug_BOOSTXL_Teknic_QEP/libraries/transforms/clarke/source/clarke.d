8ad0f918c5d8565f9d86477d38e6067586bda3ea
Jose Bail <jose.ars.bail@gmail.com>
2026-05-31 21:51:28 -0300

    gitignore

--- foc_f28379d/Debug_BOOSTXL_Teknic_QEP/libraries/transforms/clarke/source/clarke.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-libraries/transforms/clarke/source/clarke.obj: ../libraries/transforms/clarke/source/clarke.c
-
-../libraries/transforms/clarke/source/clarke.c:
-
--- foc_f28379d/Debug_BOOSTXL_Teknic_QEP/subdir_vars.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
-LIB_SRCS += \
-/home/jose/ti/c2000/C2000Ware_26_01_00_00/driverlib/f2837xd/driverlib/ccs/Release/driverlib.lib 
-
-
